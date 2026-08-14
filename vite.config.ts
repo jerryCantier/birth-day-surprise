4e090fa76d35521c085c2355bc442e4a869b77f0
jerryCantier <jerry.pante@cantier.com>
2026-08-14 09:57:32 +0800

    Add Tailwind CSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev
 export default defineConfig({
   base: '/birth-day-surprise/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
