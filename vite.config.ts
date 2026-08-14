317ec4bb5f988000c0af801bd5a7745898dae587
jerryCantier <jerry.pante@cantier.com>
2026-08-14 09:46:33 +0800

    Update Vite React plugin import statement

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin' // or vue, depending on your setup
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev
 export default defineConfig({
   base: '/birth-day-surprise/',
   plugins: [react()],
